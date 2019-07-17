97b9c74126d155ddbcd5353dadfafbd1a8ec8827
John Dunn <jdunn33@cnm.edu>
2019-07-17 09:58:44 -0600

    , changed prefix for table id, added new ERD table, added PK/FK, added ERD image. removed variables, added ERD7, added ERD8, changed foreign key assignement. added sql directory, reset drop table statements, changed orientation of table

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,29 +1,31 @@
 ALTER DATABASE jdunn33 CHAR SET utf8 COLLATE utf8_unicode_ci;
 
-DROP TABLE if exists account;
 DROP TABLE if exists relation;
+DROP TABLE if exists account;
 DROP TABLE if exists user;
 
 
 CREATE TABLE user(
-   userId BINARY(16) not null,
-   userName VARCHAR(255) not null ,
-   userHash char(97) not null ,
-   userEmail VARCHAR(128) not null,
-   unique (userEmail),
-   primary key (userId)
-);
-
-CREATE TABLE relation(
-   relationUserId BINARY(16) not null,
-   relationType char(32) not null,
-   relationAccountId BINARY(16) not null,
-   FOREIGN KEY (relationUserId) REFERENCES user(userId)
+	userId BINARY(16) not null,
+	userName VARCHAR(255) not null ,
+	userHash VARCHAR(97) not null ,
+	userEmail VARCHAR(128) not null,
+	unique (userEmail),
+	primary key (userId)
 );
 
 CREATE TABLE account(
-	accountNumber CHAR(97) not null,
+   accountNumber VARCHAR(97) not null,
 	accountDue VARCHAR(32) not null,
 	accountId BINARY(16) not null,
-	INDEX (accountId)
+	INDEX (accountId),
+	unique (accountnumber)
+);
+
+CREATE TABLE relation(
+	relationUserId BINARY(16) not null,
+	relationType CHAR(16) not null,
+	relationAccountId BINARY(16) not null,
+	FOREIGN KEY (relationAccountId) REFERENCES account(accountid),
+	FOREIGN KEY (relationUserId) REFERENCES user(userId)
 );
